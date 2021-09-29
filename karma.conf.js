2c3b31e57c9c5f7a799b3c5edf66dd4ec45541a8
SkyblockTools <lucas.j.tan@gmail.com>
2021-09-28 18:50:38 -0700

    stop being stupid tillbot

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/altoponix'),
+      dir: require('path').join(__dirname, './coverage/altoponixclub.github.io'),
       subdir: '.',
       reporters: [
         { type: 'html' },
