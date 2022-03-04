db01786d59d8620e2d848d166a6b13bda0589d17
tillvit <lucas.j.tan@gmail.com>
2022-03-03 18:32:50 -0800

    Placeholder please edit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Sponsor {
+  description: string,
+  img: string,
+  url: string
+}
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -8,6 +14,33 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   imageURLs: String[] = ["../assets/s3.png", "../assets/s2.png", "../assets/s1.png"];
+
+  sponsorOrgsGold: Sponsor[] = [
+    {description: `<b>ODriveRobotics</b> produces high end, 
+    high performance BLDC motor controllers, 
+    used by top robotics research organizations 
+    and educational institutions. They sponsor our 
+    team by providing us free parts totalling hundreds of dollars, 
+    making it possible for our team to use these standard industry 
+    tools in our robotics solutions. <b>ODriveRobotics</b> continues to 
+    drive forth innovation in the field of robotics, including 
+    partnering with AltoPonix.`, img: "../assets/sponsors/odrive.png", url: "https://odriverobotics.com"},
+    {description: "hello i am a test sponsor", img: "../assets/sponsors/odrive.png", url: "https://google.com"},
+  ]
+
+  sponsorOrgsSilver: Sponsor[] = [
+    {description: "hello i am a test sponsor", img: "../assets/sponsors/odrive.png", url: "https://google.com"},
+    {description: "hello i am a test sponsor", img: "../assets/sponsors/odrive.png", url: "https://google.com"},
+  ]
+
+  sponsorOrgsGreen: Sponsor[] = [
+    {description: "hello i am a test sponsor", img: "../assets/sponsors/odrive.png", url: "https://google.com"},
+    {description: "hello i am a test sponsor", img: "../assets/sponsors/odrive.png", url: "https://google.com"},
+  ]
+
+  sponsorIndividualsGold: String[] = ["john test"]
+  sponsorIndividualsSilver: String[] = ["john test"]
+  sponsorIndividualsGreen: String[] = ["john test"]
   
   constructor() { }
 
